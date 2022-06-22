fbfcc9dfb587560399617a04f72f5818fb8ad609
William Camargo <44587916+hotwyl@users.noreply.github.com>
2022-06-21 22:56:35 -0300

    ajustes calculos e somatorios teste

--- function.php
+++ function.php
@@ -4,8 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["dados"]) && !empty($_POST["dados"])) {
 
     foreach ($_POST["dados"] as $key => $values) {
@@ -14,10 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["dados"]) && !empty($_P
         }
     }
 
-    $informacoes = getInformacoes($arDados['contato']);
-    $questoes = getQuestoes($arDados['questions']);
+    $val['informacoes'] = getInformacoes($arDados['contato']);
+    $val['questoes'] = getQuestoes($arDados['questions']);
 
-    $salva = setBanco($informacoes, $questoes);
+    $salva = setBanco($val['informacoes'], $val['questoes']);
     // $leadLovers = sendlead();
     // $email = sendEmail();
 
@@ -33,43 +31,40 @@ function getInformacoes($dados)
 function getQuestoes($dados)
 {
     foreach ($dados as $key => $value) {
-        $val[$key] = intval($value);
+        $val['questao'][$key] = intval($value);
     }
 
-    $val['section1'] = intval($val['question1']) + intval($val['question2']) + intval($val['question3']) + intval($val['question4']) + intval($val['question5']) + intval($val['question6']);
-    $val['section2'] = intval($val['question7']) + intval($val['question8']) + intval($val['question9']) + intval($val['question10']) + intval($val['question11']) + intval($val['question12']);
-    $val['section3'] = intval($val['question13']) + intval($val['question14']) + intval($val['question15']) + intval($val['question16']) + intval($val['question17']) + intval($val['question18']);
-    $val['section4'] = intval($val['question19']) + intval($val['question20']) + intval($val['question21']) + intval($val['question22']) + intval($val['question23']) + intval($val['question24']);
-    $val['section5'] = intval($val['question25']) + intval($val['question26']) + intval($val['question27']) + intval($val['question28']) + intval($val['question29']) + intval($val['question30']);
-    $val['section6'] = intval($val['question31']) + intval($val['question32']) + intval($val['question33']) + intval($val['question34']) + intval($val['question35']) + intval($val['question36']);
-    $val['section7'] = intval($val['question37']) + intval($val['question38']) + intval($val['question39']) + intval($val['question40']) + intval($val['question41']) + intval($val['question42']);
-    $val['section8'] = intval($val['question43']) + intval($val['question44']) + intval($val['question45']) + intval($val['question46']) + intval($val['question47']) + intval($val['question48']);
-    $val['section9'] = intval($val['question49']) + intval($val['question50']) + intval($val['question51']) + intval($val['question52']) + intval($val['question53']) + intval($val['question54']);
-    $val['section10'] = intval($val['question55']) + intval($val['question56']) + intval($val['question57']) + intval($val['question58']) + intval($val['question59']) + intval($val['question60']);
-    $val['section11'] = intval($val['question61']) + intval($val['question62']) + intval($val['question63']) + intval($val['question64']) + intval($val['question65']) + intval($val['question66']);
-    $val['section12'] = intval($val['question67']) + intval($val['question68']) + intval($val['question69']) + intval($val['question70']) + intval($val['question71']) + intval($val['question72']);
-    $val['section13'] = intval($val['question73']) + intval($val['question74']) + intval($val['question75']) + intval($val['question76']) + intval($val['question77']) + intval($val['question78']);
-    $val['section14'] = intval($val['question79']) + intval($val['question80']) + intval($val['question81']) + intval($val['question82']) + intval($val['question83']) + intval($val['question84']);
+    $val['section']['section1'] = intval($val['questao']['question1']) + intval($val['questao']['question2']) + intval($val['questao']['question3']) + intval($val['questao']['question4']) + intval($val['questao']['question5']) + intval($val['questao']['question6']);
+    $val['section']['section2'] = intval($val['questao']['question7']) + intval($val['questao']['question8']) + intval($val['questao']['question9']) + intval($val['questao']['question10']) + intval($val['questao']['question11']) + intval($val['questao']['question12']);
+    $val['section']['section3'] = intval($val['questao']['question13']) + intval($val['questao']['question14']) + intval($val['questao']['question15']) + intval($val['questao']['question16']) + intval($val['questao']['question17']) + intval($val['questao']['question18']);
+    $val['section']['section4'] = intval($val['questao']['question19']) + intval($val['questao']['question20']) + intval($val['questao']['question21']) + intval($val['questao']['question22']) + intval($val['questao']['question23']) + intval($val['questao']['question24']);
+    $val['section']['section5'] = intval($val['questao']['question25']) + intval($val['questao']['question26']) + intval($val['questao']['question27']) + intval($val['questao']['question28']) + intval($val['questao']['question29']) + intval($val['questao']['question30']);
+    $val['section']['section6'] = intval($val['questao']['question31']) + intval($val['questao']['question32']) + intval($val['questao']['question33']) + intval($val['questao']['question34']) + intval($val['questao']['question35']) + intval($val['questao']['question36']);
+    $val['section']['section7'] = intval($val['questao']['question37']) + intval($val['questao']['question38']) + intval($val['questao']['question39']) + intval($val['questao']['question40']) + intval($val['questao']['question41']) + intval($val['questao']['question42']);
+    $val['section']['section8'] = intval($val['questao']['question43']) + intval($val['questao']['question44']) + intval($val['questao']['question45']) + intval($val['questao']['question46']) + intval($val['questao']['question47']) + intval($val['questao']['question48']);
+    $val['section']['section9'] = intval($val['questao']['question49']) + intval($val['questao']['question50']) + intval($val['questao']['question51']) + intval($val['questao']['question52']) + intval($val['questao']['question53']) + intval($val['questao']['question54']);
+    $val['section']['section10'] = intval($val['questao']['question55']) + intval($val['questao']['question56']) + intval($val['questao']['question57']) + intval($val['questao']['question58']) + intval($val['questao']['question59']) + intval($val['questao']['question60']);
+    $val['section']['section11'] = intval($val['questao']['question61']) + intval($val['questao']['question62']) + intval($val['questao']['question63']) + intval($val['questao']['question64']) + intval($val['questao']['question65']) + intval($val['questao']['question66']);
+    $val['section']['section12'] = intval($val['questao']['question67']) + intval($val['questao']['question68']) + intval($val['questao']['question69']) + intval($val['questao']['question70']) + intval($val['questao']['question71']) + intval($val['questao']['question72']);
+    $val['section']['section13'] = intval($val['questao']['question73']) + intval($val['questao']['question74']) + intval($val['questao']['question75']) + intval($val['questao']['question76']) + intval($val['questao']['question77']) + intval($val['questao']['question78']);
+    $val['section']['section14'] = intval($val['questao']['question79']) + intval($val['questao']['question80']) + intval($val['questao']['question81']) + intval($val['questao']['question82']) + intval($val['questao']['question83']) + intval($val['questao']['question84']);
 
-    $val['afrodite'] = intval($val['question1']) + intval($val['question7']) + intval($val['question13']) + intval($val['question19']) + intval($val['question25']) + intval($val['question31']) + intval($val['question37']) + intval($val['question43']) + intval($val['question49']) + intval($val['question55']) + intval($val['question61']) + intval($val['question67']) + intval($val['question73']) + intval($val['question79']);
-    $val['artemis'] = intval($val['question2']) + intval($val['question8']) + intval($val['question14']) + intval($val['question20']) + intval($val['question26']) + intval($val['question32']) + intval($val['question38']) + intval($val['question44']) + intval($val['question50']) + intval($val['question56']) + intval($val['question62']) + intval($val['question68']) + intval($val['question74']) + intval($val['question80']);
-    $val['atenas'] = intval($val['question3']) + intval($val['question9']) + intval($val['question15']) + intval($val['question21']) + intval($val['question27']) + intval($val['question33']) + intval($val['question39']) + intval($val['question45']) + intval($val['question51']) + intval($val['question57']) + intval($val['question63']) + intval($val['question69']) + intval($val['question75']) + intval($val['question81']);
-    $val['hera'] = intval($val['question4']) + intval($val['question10']) + intval($val['question16']) + intval($val['question22']) + intval($val['question28']) + intval($val['question34']) + intval($val['question40']) + intval($val['question46']) + intval($val['question52']) + intval($val['question58']) + intval($val['question64']) + intval($val['question70']) + intval($val['question76']) + intval($val['question82']);
-    $val['persefone'] = intval($val['question5']) + intval($val['question11']) + intval($val['question17']) + intval($val['question23']) + intval($val['question29']) + intval($val['question35']) + intval($val['question41']) + intval($val['question47']) + intval($val['question53']) + intval($val['question59']) + intval($val['question65']) + intval($val['question71']) + intval($val['question77']) + intval($val['question83']);
-    $val['demeter'] = intval($val['question6']) + intval($val['question12']) + intval($val['question18']) + intval($val['question24']) + intval($val['question30']) + intval($val['question36']) + intval($val['question42']) + intval($val['question48']) + intval($val['question54']) + intval($val['question60']) + intval($val['question66']) + intval($val['question72']) + intval($val['question78']) + intval($val['question84']);
+    $val['teste']['atena'] = intval($val['questao']['question6']) + intval($val['questao']['question7']) + intval($val['questao']['question14']) + intval($val['questao']['question21']) + intval($val['questao']['question28']) + intval($val['questao']['question34']) + intval($val['questao']['question42']) + intval($val['questao']['question45']) + intval($val['questao']['question52']) + intval($val['questao']['question59']) + intval($val['questao']['question61']) + intval($val['questao']['question69']) + intval($val['questao']['question78']) + intval($val['questao']['question79']);
+    $val['teste']['afrodite'] = intval($val['questao']['question5']) + intval($val['questao']['question9']) + intval($val['questao']['question17']) + intval($val['questao']['question20']) + intval($val['questao']['question30']) + intval($val['questao']['question31']) + intval($val['questao']['question39']) + intval($val['questao']['question44']) + intval($val['questao']['question53']) + intval($val['questao']['question56']) + intval($val['questao']['question64']) + intval($val['questao']['question72']) + intval($val['questao']['question77']) + intval($val['questao']['question80']);
+    $val['teste']['persefone'] = intval($val['questao']['question3']) + intval($val['questao']['question10']) + intval($val['questao']['question16']) + intval($val['questao']['question24']) + intval($val['questao']['question25']) + intval($val['questao']['question35']) + intval($val['questao']['question37']) + intval($val['questao']['question48']) + intval($val['questao']['question54']) + intval($val['questao']['question55']) + intval($val['questao']['question62']) + intval($val['questao']['question70']) + intval($val['questao']['question75']) + intval($val['questao']['question81']);
+    $val['teste']['artemis'] = intval($val['questao']['question2']) + intval($val['questao']['question8']) + intval($val['questao']['question18']) + intval($val['questao']['question19']) + intval($val['questao']['question29']) + intval($val['questao']['question33']) + intval($val['questao']['question40']) + intval($val['questao']['question47']) + intval($val['questao']['question49']) + intval($val['questao']['question57']) + intval($val['questao']['question66']) + intval($val['questao']['question68']) + intval($val['questao']['question76']) + intval($val['questao']['question82']);
+    $val['teste']['demeter'] = intval($val['questao']['question1']) + intval($val['questao']['question12']) + intval($val['questao']['question15']) + intval($val['questao']['question22']) + intval($val['questao']['question27']) + intval($val['questao']['question32']) + intval($val['questao']['question41']) + intval($val['questao']['question46']) + intval($val['questao']['question50']) + intval($val['questao']['question60']) + intval($val['questao']['question65']) + intval($val['questao']['question67']) + intval($val['questao']['question73']) + intval($val['questao']['question83']);
+    $val['teste']['hera'] = intval($val['questao']['question4']) + intval($val['questao']['question11']) + intval($val['questao']['question13']) + intval($val['questao']['question23']) + intval($val['questao']['question26']) + intval($val['questao']['question36']) + intval($val['questao']['question38']) + intval($val['questao']['question43']) + intval($val['questao']['question51']) + intval($val['questao']['question58']) + intval($val['questao']['question63']) + intval($val['questao']['question71']) + intval($val['questao']['question74']) + intval($val['questao']['question84']);
 
-    $val['total1'] = intval($val['section1']) + intval($val['section2']) + intval($val['section3']) + intval($val['section4']) + intval($val['section5']) + intval($val['section6']) + intval($val['section7']) + intval($val['section8']) + intval($val['section9']) + intval($val['section10']) + intval($val['section11']) + intval($val['section12']) + intval($val['section13']) + intval($val['section14']);
-    $val['total2'] = intval($val['afrodite']) + intval($val['artemis']) + intval($val['atenas']) + intval($val['hera']) + intval($val['persefone']) + intval($val['demeter']);
+    $val['pontuacao']['geral'] = intval($val['section']['section1']) + intval($val['section']['section2']) + intval($val['section']['section3']) + intval($val['section']['section4']) + intval($val['section']['section5']) + intval($val['section']['section6']) + intval($val['section']['section7']) + intval($val['section']['section8']) + intval($val['section']['section9']) + intval($val['section']['section10']) + intval($val['section']['section11']) + intval($val['section']['section12']) + intval($val['section']['section13']) + intval($val['section']['section14']);
+    $val['pontuacao']['Total'] = intval($val['teste']['atena']) + intval($val['teste']['afrodite']) + intval($val['teste']['persefone']) + intval($val['teste']['artemis']) + intval($val['teste']['demeter']) + intval($val['teste']['hera']);
 
     return $val;
 }
 
-function setBanco($informacoes, $questoes)
+function setBanco($info, $quest)
 {
-
-    echo "<pre>";
-    print_r($questoes);
-    echo "</pre>";
+    print_r(json_encode($quest));
     exit;
 
     try {
